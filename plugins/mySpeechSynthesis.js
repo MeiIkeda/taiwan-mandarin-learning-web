335740e180e4a03f710450af2dcacfcdcf050ab7
MeiIkeda <mmeeddeecchhii@yahoo.co.jp>
2020-01-20 22:46:46 +0900

    発声バリエーションに台湾追加

--- plugins/mySpeechSynthesis.js
+++ plugins/mySpeechSynthesis.js
@@ -8,8 +8,10 @@ export default {
       }
       const uttr = new SpeechSynthesisUtterance(word)
       let isChinese = false
+      let includeZhTw = false
       let includeZhCn = false
-      let includeTianTian = false
+      let includeZhTwAndroid = false
+      let includeZhCnAndroid = false
       voices.forEach(function (voice, i) {
         if (voice.lang.includes('zh')) {
           isChinese = true
@@ -17,8 +19,14 @@ export default {
         if (voice.lang.includes('zh-CN')) {
           includeZhCn = true
         }
-        if (voice.lang.includes('Tian-Tian')) {
-          includeTianTian = true
+        if (voice.lang.includes('zh-TW')) {
+          includeZhTw = true
+        }
+        if (voice.lang.includes('zh_TW_#Hant')) {
+          includeZhTwAndroid = true
+        }
+        if (voice.lang.includes('zh_CN_#Hans')) {
+          includeZhCnAndroid = true
         }
         // console.log('lang:' + voice.lang)
         // console.log('name:' + voice.name)
@@ -26,11 +34,14 @@ export default {
       if (isChinese === false) {
         const agent = window.navigator.userAgent
         return 'お使いのブラウザーは単語再生に対応していない可能性があります。(2)' + agent
-      } else if (includeTianTian === true) {
-        uttr.name = 'Tian-Tian'
-        uttr.lang = 'zh-CN'
+      } else if (includeZhTw === true) {
+        uttr.lang = 'zh-TW'
+      } else if (includeZhTwAndroid === true) {
+        uttr.lang = 'zh_TW_#Hant'
       } else if (includeZhCn === true) {
         uttr.lang = 'zh-CN'
+      } else if (includeZhCnAndroid === true) {
+        uttr.lang = 'zh_CN_#Hans'
       } else {
         uttr.lang = 'zh'
       }
