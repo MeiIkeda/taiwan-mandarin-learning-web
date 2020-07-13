2d343668ebc633c1323e072406a32d05158bbf19
MeiIkeda <mmeeddeecchhii@yahoo.co.jp>
2020-07-13 20:44:45 +0900

    fix

--- plugins/mySpeechSynthesis.js
+++ plugins/mySpeechSynthesis.js
@@ -11,43 +11,67 @@ export default {
     mySpeak (word, voices) {
       const uttr = new SpeechSynthesisUtterance(word)
       let isChinese = false
-      // let includeZhTw = false
-      let includeZhCn = false
-      // let includeZhTwAndroid = false
-      let includeZhCnAndroid = false
-      voices.forEach(function (voice, i) {
+      voices.some(function (voice) {
+        if (voice.name.includes('Mei-Jia')) {
+          uttr.name = 'Mei-Jia'
+          return true
+        }
+        if (voice.name.includes('Google 國語（臺灣）')) {
+          uttr.name = 'Google 國語（臺灣）'
+          return true
+        }
+        if (voice.name.includes('國語（臺灣）')) {
+          uttr.name = '國語（臺灣）'
+          return true
+        }
+        if (voice.name.includes('中国語 台湾')) {
+          uttr.name = '中国語 台湾'
+          return true
+        }
+        if (voice.name.includes('中国語 中国')) {
+          uttr.name = '中国語 中国'
+          return true
+        }
+        if (voice.name.includes('Yu-shu')) {
+          uttr.name = 'Yu-shu'
+          return true
+        }
+        if (voice.name.includes('Google 普通话（中国大陆）')) {
+          uttr.name = 'Google 普通话（中国大陆）'
+          return true
+        }
+        if (voice.name.includes('普通话（中国大陆）')) {
+          uttr.name = '普通话（中国大陆）'
+          return true
+        }
+      })
+      voices.some(function (voice) {
         if (voice.lang.includes('zh')) {
           isChinese = true
         }
+        if (voice.lang.includes('zh-TW')) {
+          uttr.lang = 'zh-TW'
+          return true
+        }
+        if (voice.lang.includes('zh_TW_#Hant')) {
+          uttr.lang = 'zh_TW_#Hant'
+          return true
+        }
         if (voice.lang.includes('zh-CN')) {
-          includeZhCn = true
+          uttr.lang = 'zh-CN'
+          return true
         }
-        // if (voice.lang.includes('zh-TW')) {
-        //   includeZhTw = true
-        // }
-        // if (voice.lang.includes('zh_TW_#Hant')) {
-        //   includeZhTwAndroid = true
-        // }
         if (voice.lang.includes('zh_CN_#Hans')) {
-          includeZhCnAndroid = true
+          uttr.lang = 'zh_CN_#Hans'
+          return true
         }
-        // console.log('lang:' + voice.lang)
-        // console.log('name:' + voice.name)
       })
       if (isChinese === false) {
         const agent = window.navigator.userAgent
         return 'お使いのブラウザーは単語再生に対応していない可能性があります。(2)' + agent
-        // } else if (includeZhTw === true) {
-        //   uttr.lang = 'zh-TW'
-        // } else if (includeZhTwAndroid === true) {
-        //   uttr.lang = 'zh_TW_#Hant'
-      } else if (includeZhCn === true) {
-        uttr.lang = 'zh-CN'
-      } else if (includeZhCnAndroid === true) {
-        uttr.lang = 'zh_CN_#Hans'
-      } else {
-        uttr.lang = 'zh'
       }
+      console.log('lang=' + uttr.lang)
+      console.log('name=' + uttr.name)
       speechSynthesis.cancel()
       speechSynthesis.speak(uttr)
       return 'success'
