4f18219ece9a6342e607e47faac52a2181b03cf2
MeiIkeda <mmeeddeecchhii@yahoo.co.jp>
2020-07-30 01:24:24 +0900

    Added "Text1-Lesson5"

--- pages/sentence/index.vue
+++ pages/sentence/index.vue
@@ -52,6 +52,10 @@
           <nuxt-link :to="{ name: 'sentence-text1-lesson4' }" style="text-align: center">
             當代中文課程課1・第4章
           </nuxt-link>
+          <br>
+          <nuxt-link :to="{ name: 'sentence-text1-lesson5' }" style="text-align: center">
+            當代中文課程課1・第5章
+          </nuxt-link>
         </p>
         <br>
       </div>
