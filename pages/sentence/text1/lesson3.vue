ad592887dd0d34a8d9585264e0bf19e3bd8d668b
MeiIkeda <mmeeddeecchhii@yahoo.co.jp>
2020-09-13 01:41:28 +0900

    fixed Lesson3

--- pages/sentence/text1/lesson3.vue
+++ pages/sentence/text1/lesson3.vue
@@ -26,9 +26,9 @@
           ・週末
         </p>
         <SentenceFormat
-          :sentence="'週末我去散步還是喝酒.'"
-          :bopomofo="'ㄓㄡ ㄇㄛˋ ㄨㄛˇ ㄑㄩˋ ㄙㄢˋ ㄅㄨˋ ㄏㄞˊ ㄕˋ ㄏㄜ ㄐㄧㄡˇ.'"
-          :pinyin="'zhou1 mo4 wo3 qu4 san4 bu4 hai2 shi4 he1 jiu3.'"
+          :sentence="'週末我去散步或是喝酒。'"
+          :bopomofo="'ㄓㄡ ㄇㄛˋ ㄨㄛˇ ㄑㄩˋ ㄙㄢˋ ㄅㄨˋ ㄏㄨㄛˋ ㄕˋ ㄏㄜ ㄐㄧㄡˇ.'"
+          :pinyin="'zhou1 mo4 wo3 qu4 san4 bu4 huo4 shi4 he1 jiu3.'"
           :translation="'私は週末に散歩をしたり飲みに行ったりします。'"
         />
         <br>
@@ -36,7 +36,7 @@
           ・聽
         </p>
         <SentenceFormat
-          :sentence="'那個聽起來很好玩.'"
+          :sentence="'那個聽起來很好玩。'"
           :bopomofo="'ㄋㄚˋ ㄍㄜˋ ㄊㄧㄥ ㄑㄧˇ ㄌㄞˊ ㄏㄣˇ ㄏㄠˇ ㄨㄢˊ.'"
           :pinyin="'na4 ge4 ting1 qi3 lai2 hen3 hao3 wan2.'"
           :translation="'それは楽しそうですね。'"
@@ -46,7 +46,7 @@
           ・音樂
         </p>
         <SentenceFormat
-          :sentence="'運動的時候, 我喜歡聽音樂.'"
+          :sentence="'運動的時候, 我喜歡聽音樂。'"
           :bopomofo="'ㄩㄣˋ ㄉㄨㄥˋ ㄉㄜ˙ ㄕˊ ㄏㄡˋ, ㄨㄛˇ ㄒㄧˇ ㄏㄨㄢ ㄊㄧㄥ ㄧㄣ ㄩㄝˋ.'"
           :pinyin="'yun4 dong4 de shi2 hou4, wo3 xi3 huan1 ting1 yin1 yue4.'"
           :translation="'運動するときは音楽を聞くのが好きです。'"
@@ -56,7 +56,7 @@
           ・運動
         </p>
         <SentenceFormat
-          :sentence="'我不擅長運動.'"
+          :sentence="'我不擅長運動。'"
           :bopomofo="'ㄨㄛˇ ㄅㄨˋ ㄕㄢˋ ㄔㄤˊ ㄩㄣˋ ㄉㄨㄥˋ.'"
           :pinyin="'wo3 bu4 shan4 chang2 yun4 dong4.'"
           :translation="'私は運動が苦手です。'"
@@ -66,7 +66,7 @@
           ・打
         </p>
         <SentenceFormat
-          :sentence="'白天打籃球很熱.'"
+          :sentence="'白天打籃球很熱。'"
           :bopomofo="'ㄅㄞˊ ㄊㄧㄢ ㄉㄚˇ ㄌㄢˊ ㄑㄧㄡˊ ㄏㄣˇ ㄖㄜˋ.'"
           :pinyin="'bai2 tian1 da3 lan2 qiu2 hen3 re4.'"
           :translation="'昼間にバスケをするのは暑いです。'"
@@ -76,7 +76,7 @@
           ・網球
         </p>
         <SentenceFormat
-          :sentence="'網球是一項時尚運動.'"
+          :sentence="'網球是一項時尚運動。'"
           :bopomofo="'ㄨㄤˇ ㄑㄧㄡˊ ㄕˋ ㄧ ㄒㄧㄤˋ ㄕˊ ㄕㄤˋ ㄩㄣˋ ㄉㄨㄥˋ.'"
           :pinyin="'wang3 qiu2 shi4 yi1 xiang4 shi2 shang4 yun4 dong4.'"
           :translation="'テニスはおしゃれなスポーツです。'"
@@ -86,9 +86,9 @@
           ・棒球
         </p>
         <SentenceFormat
-          :sentence="'棒球用一個球和一枝棒.'"
-          :bopomofo="'ㄅㄤˋ ㄑㄧㄡˊ ㄩㄥˋ ㄧˊ ㄍㄜˋ ㄑㄧㄡˊ ㄏㄢˋ ㄧ ㄓ ㄅㄤˋ.'"
-          :pinyin="'bang4 qiu2 yong4 yi1 ge4 qiu2 han4 yi1 zhi1 bang4.'"
+          :sentence="'棒球是一種團隊運動。'"
+          :bopomofo="'ㄅㄤˋ ㄑㄧㄡˊ ㄕˋ ㄧ ㄓㄨㄥˇ ㄊㄨㄢˊ ㄉㄨㄟˋ ㄩㄣˋ ㄉㄨㄥˋ.'"
+          :pinyin="'bang4 qiu2 shi4 yi1 zhong3 tuan2 dui4 yun4 dong4.'"
           :translation="'野球はボール1個とバット1本を使います。'"
         />
         <br>
@@ -96,7 +96,7 @@
           ・和
         </p>
         <SentenceFormat
-          :sentence="'我要吃牛肉湯和白飯.'"
+          :sentence="'我要吃牛肉湯和白飯。'"
           :bopomofo="'ㄨㄛˇ ㄧㄠˋ ㄔ ㄋㄧㄡˊ ㄖㄡˋ ㄊㄤ ㄏㄢˋ ㄅㄞˊ ㄈㄢˋ.'"
           :pinyin="'wo3 yao4 chi1 niu2 rou4 tang1 han4 bai2 fan4.'"
           :translation="'牛肉湯と白いご飯が食べたいです。'"
@@ -106,7 +106,7 @@
           ・游泳
         </p>
         <SentenceFormat
-          :sentence="'我要買新的游泳衣.'"
+          :sentence="'我要買新的游泳衣。'"
           :bopomofo="'ㄨㄛˇ ㄧㄠˋ ㄇㄞˇ ㄒㄧㄣ ㄉㄜ˙ ㄧㄡˊ ㄩㄥˇ ㄧ.'"
           :pinyin="'wo3 yao4 mai3 xin1 de you2 yong3 yi1.'"
           :translation="'新しい水着を買いたいです。'"
@@ -116,7 +116,7 @@
           ・常
         </p>
         <SentenceFormat
-          :sentence="'太常下雨.'"
+          :sentence="'太常下雨。'"
           :bopomofo="'ㄊㄞˋ ㄔㄤˊ ㄒㄧㄚˋ ㄩˇ.'"
           :pinyin="'tai4 chang2 xia4 yu3.'"
           :translation="'雨が降りすぎる。'"
@@ -126,7 +126,7 @@
           ・籃球
         </p>
         <SentenceFormat
-          :sentence="'他的朋友是籃球選手.'"
+          :sentence="'他的朋友是籃球選手。'"
           :bopomofo="'ㄊㄚ ㄉㄜ˙ ㄆㄥˊ ㄧㄡˇ ㄕˋ ㄌㄢˊ ㄑㄧㄡˊ ㄒㄩㄢˇ ㄕㄡˇ.'"
           :pinyin="'ta1 de peng2 you3 shi4 lan2 qiu2 xuan3 shou3.'"
           :translation="'彼の友達はバスケットボール選手です.'"
@@ -146,7 +146,7 @@
           ・踢
         </p>
         <SentenceFormat
-          :sentence="'我不小心踢了那隻狗.'"
+          :sentence="'我不小心踢了那隻狗。'"
           :bopomofo="'ㄨㄛˇ ㄅㄨˋ ㄒㄧㄠˇ ㄒㄧㄣ ㄊㄧ ㄌㄜ˙ ㄋㄚˋ ㄓˇ ㄍㄡˇ.'"
           :pinyin="'wo3 bu4 xiao3 xin1 ti1 le na4 zhi1 go3u.'"
           :translation="'誤って犬を蹴ってしまいました。'"
@@ -156,7 +156,7 @@
           ・足球
         </p>
         <SentenceFormat
-          :sentence="'下課後, 我想去踢足球.'"
+          :sentence="'下課後, 我想去踢足球。'"
           :bopomofo="'ㄒㄧㄚˋ ㄎㄜˋ ㄏㄡˋ, ㄨㄛˇ ㄒㄧㄤˇ ㄑㄩˋ ㄊㄧ ㄗㄨˊ ㄑㄧㄡˊ.'"
           :pinyin="'xia4 ke4 hou4, wo3 xiang3 qu4 ti1 zu2 qiu2.'"
           :translation="'放課後はサッカーをしたいです。'"
@@ -166,7 +166,7 @@
           ・覺得
         </p>
         <SentenceFormat
-          :sentence="'我覺得那個菜比較好吃.'"
+          :sentence="'我覺得那個菜比較好吃。'"
           :bopomofo="'ㄨㄛˇ ㄐㄩㄝˊ ㄉㄜ˙ ㄋㄚˋ ㄍㄜˋ ㄘㄞˋ ㄅㄧˇ ㄐㄧㄠˋ ㄏㄠˇ ㄔ.'"
           :pinyin="'wo3 jue2 de na4 ge4 cai4 bi3 jiao4 hao3 chi1.'"
           :translation="'私はあの料理の方が美味しいと思います。'"
@@ -176,7 +176,7 @@
           ・好玩
         </p>
         <SentenceFormat
-          :sentence="'上週末, 我們去很好玩的地方了.'"
+          :sentence="'上週末, 我們去很好玩的地方了。'"
           :bopomofo="'ㄕㄤˋ ㄓㄡ ㄇㄛˋ, ㄨㄛˇ ㄇㄣ˙ ㄑㄩˋ ㄏㄣˇ ㄏㄠˇ ㄨㄢˊ ㄉㄜ˙ ㄉㄧˋ ㄈㄤ ㄌㄜ˙.'"
           :pinyin="'shang4 zhou1 mo4, wo3 men qu4 hen3 hao3 wan2 de di4 fang1 le.'"
           :translation="'先週末、楽しい場所に行きました。'"
@@ -186,7 +186,7 @@
           ・明天
         </p>
         <SentenceFormat
-          :sentence="'明天早上, 我們一起吃早飯吧.'"
+          :sentence="'明天早上, 我們一起吃早飯吧。'"
           :bopomofo="'ㄇㄧㄥˊ ㄊㄧㄢ ㄗㄠˇ ㄕㄤˋ, ㄨㄛˇ ㄇㄣ˙ ㄧˋ ㄑㄧˇ ㄔ ㄗㄠˇ ㄈㄢˋ ㄅㄚ˙.'"
           :pinyin="'ming2 tian1 zao3 shang4, wo3 men yi1 qi3 chi1 zao3 fan4 ba.'"
           :translation="'明日の朝、一緒に朝食を食べましょう。'"
@@ -196,7 +196,7 @@
           ・早上
         </p>
         <SentenceFormat
-          :sentence="'我最喜歡早上游泳.'"
+          :sentence="'我最喜歡早上游泳。'"
           :bopomofo="'ㄨㄛˇ ㄗㄨㄟˋ ㄒㄧˇ ㄏㄨㄢ ㄗㄠˇ ㄕㄤˋ ㄧㄡˊ ㄩㄥˇ.'"
           :pinyin="'wo3 zui4 xi3 huan1 zao3 shang4 you2 yong3.'"
           :translation="'私は朝の水泳が一番好きです。'"
@@ -219,11 +219,11 @@
           :sentence="'我買新的衣服了, 怎麼樣?'"
           :bopomofo="'ㄨㄛˇ ㄇㄞˇ ㄒㄧㄣ ㄉㄜ˙ ㄧ ㄈㄨˊ ㄌㄜ˙, ㄗㄣˇ ㄇㄜ˙ ㄧㄤˋ?'"
           :pinyin="'wo3 mai3 xin1 de yi1 fu2 le. zen3 me yang4?'"
-          :translation="'新しい服を買いましたが, どうですか？?'"
+          :translation="'新しい服を買いましたが, どうですか?'"
         />
         <br>
         <p>
-          ・阿
+          ・啊
         </p>
         <SentenceFormat
           :sentence="'我們去買麵包, 怎麼樣? 好啊!'"
@@ -256,9 +256,9 @@
           ・今天
         </p>
         <SentenceFormat
-          :sentence="'今天我有頭痛.'"
-          :bopomofo="'ㄐㄧㄣ ㄊㄧㄢ ㄨㄛˇ ㄧㄡˇ ㄊㄡˊ ㄊㄨㄥˋ.'"
-          :pinyin="'jin1 tian1 wo3 you3 tou2 tong4.'"
+          :sentence="'今天我頭痛。'"
+          :bopomofo="'ㄐㄧㄣ ㄊㄧㄢ ㄨㄛˇ ㄊㄡˊ ㄊㄨㄥˋ.'"
+          :pinyin="'jin1 tian1 wo3 tou2 tong4.'"
           :translation="'今日は頭痛がします。'"
         />
         <br>
@@ -276,7 +276,7 @@
           ・看
         </p>
         <SentenceFormat
-          :sentence="'他現在在房間看電視.'"
+          :sentence="'他現在在房間看電視。'"
           :bopomofo="'ㄊㄚ ㄒㄧㄢˋ ㄗㄞˋ ㄗㄞˋ ㄈㄤˊ ㄐㄧㄢ ㄎㄢˋ ㄉㄧㄢˋ ㄕˋ.'"
           :pinyin="'ta1 xian4 zai4 zai4 fang2 jian1 kan4 dian4 shi4.'"
           :translation="'彼は今部屋でテレビを見ています。'"
@@ -286,7 +286,7 @@
           ・電影
         </p>
         <SentenceFormat
-          :sentence="'這週末很多人去看電影.'"
+          :sentence="'這週末很多人去看電影。'"
           :bopomofo="'ㄓㄜˋ ㄓㄡ ㄇㄛˋ ㄏㄣˇ ㄉㄨㄛ ㄖㄣˊ ㄑㄩˋ ㄎㄢˋ ㄉㄧㄢˋ ㄧㄥˇ.'"
           :pinyin="'zhe4 zhou1 mo4 hen3 duo1 ren2 qu4 kan4 dian4 ying3.'"
           :translation="'今週末多くの人が映画を見に行きます。'"
@@ -306,7 +306,7 @@
           ・想
         </p>
         <SentenceFormat
-          :sentence="'台灣的菜很好吃, 我都想吃.'"
+          :sentence="'台灣的菜很好吃, 我都想吃。'"
           :bopomofo="'ㄊㄞˊ ㄨㄢ ㄉㄜ˙ ㄘㄞˋ ㄏㄣˇ ㄏㄠˇ ㄔ, ㄨㄛˇ ㄉㄡ ㄒㄧㄤˇ ㄔ.'"
           :pinyin="'tai2 wan1 de cai4 hen3 hao3 chi1, wo3 dou1 xiang3 chi1.'"
           :translation="'台湾料理はとても美味しい。 全部食べたいです。'"
@@ -329,26 +329,26 @@
           :sentence="'我們去貓咖啡店吧?'"
           :bopomofo="'ㄨㄛˇ ㄇㄣ˙ ㄑㄩˋ ㄇㄠ ㄎㄚ ㄈㄟ ㄉㄧㄢˋ ㄅㄚ˙?'"
           :pinyin="'wo3 men qu4 mao1 ka1 fe1i dian4 ba?'"
-          :translation="'猫カフェに行きませんか？'"
+          :translation="'猫カフェに行きませんか?'"
         />
         <br>
         <p>
           ・可以
         </p>
         <SentenceFormat
-          :sentence="'我可以練習中文這裡嗎?'"
-          :bopomofo="'ㄨㄛˇ ㄎㄜˇ ㄧˇ ㄌㄧㄢˋ ㄒㄧˊ ㄓㄨㄥ ㄨㄣˊ ㄓㄜˋ ㄌㄧˇㄇㄚ˙?'"
-          :pinyin="'wo3 ke3 yi3 lian4 xi2 zhong1 wen2 zhe4 li3 ma?'"
-          :translation="'ここで中国語を練習してもいいですか？'"
+          :sentence="'我可以在這裡練習中文嗎?'"
+          :bopomofo="'ㄨㄛˇ ㄎㄜˇ ㄧˇ ㄗㄞˋ ㄓㄜˋ ㄌㄧˇ ㄌㄧㄢˋ ㄒㄧˊ ㄓㄨㄥ ㄨㄣˊ ㄇㄚ˙?'"
+          :pinyin="'wo3 ke3 yi3 zai4 zhe4 li3 lian4 xi2 zhong1 wen2 ma2?'"
+          :translation="'ここで中国語を練習してもいいですか?'"
         />
         <br>
         <p>
           ・學
         </p>
         <SentenceFormat
-          :sentence="'學中文比難學英文.'"
-          :bopomofo="'ㄒㄩㄝˊ ㄓㄨㄥ ㄨㄣˊ ㄅㄧˇ ㄋㄢˊ ㄒㄩㄝˊ ㄧㄥ ㄨㄣˊ.'"
-          :pinyin="'xue2 zhong1 wen2 bi3 nan2 xue2 ying1 wen2.'"
+          :sentence="'學中文比學英文難。'"
+          :bopomofo="'ㄒㄩㄝˊ ㄓㄨㄥ ㄨㄣˊ ㄅㄧˇ ㄒㄩㄝˊ ㄧㄥ ㄨㄣˊ ㄋㄢˊ.'"
+          :pinyin="'xue2 zhong1 wen2 bi3 xue2 ying1 wen2 nan2.'"
           :translation="'中国語を学ぶのは英語を学ぶより難しいです。'"
         />
         <br>
@@ -356,7 +356,7 @@
           ・中文
         </p>
         <SentenceFormat
-          :sentence="'日文漢字和中文漢字不一樣.'"
+          :sentence="'日文漢字和中文漢字不一樣。'"
           :bopomofo="'ㄖˋ ㄨㄣˊ ㄏㄢˋ ㄗˋ ㄏㄢˋ ㄓㄨㄥ ㄨㄣˊ ㄏㄢˋ ㄗˋ ㄅㄨˋ ㄧˊ ㄧㄤˋ.'"
           :pinyin="'ri4 wen2 han4 zi4 han4 zhong1 wen2 han4 zi4 bu4 yi1 yang4.'"
           :translation="'日本語の漢字は中国語の漢字とは異なります。'"
@@ -376,7 +376,7 @@
           ・吃
         </p>
         <SentenceFormat
-          :sentence="'我不喜歡吃香菜.'"
+          :sentence="'我不喜歡吃香菜。'"
           :bopomofo="'ㄨㄛˇ ㄅㄨˋ ㄒㄧˇ ㄏㄨㄢ ㄔ ㄒㄧㄤ ㄘㄞˋ.'"
           :pinyin="'wo3 bu4 xi3 huan1 chi1 xiang1 cai4.'"
           :translation="'パクチーは嫌いです。'"
@@ -386,7 +386,7 @@
           ・晚飯
         </p>
         <SentenceFormat
-          :sentence="'晚飯前，你要洗你的手．'"
+          :sentence="'晚飯前， 你要洗你的手。'"
           :bopomofo="'ㄨㄢˇ ㄈㄢˋ ㄑㄧㄢˊ， ㄋㄧˇ ㄧㄠˋ ㄒㄧˇ ㄋㄧˇ ㄉㄜ˙ ㄕㄡˇ.'"
           :pinyin="'wan3 fan4 qian2, ni3 yao4 xi3 ni3 de shou3.'"
           :translation="'夕飯の前に、手を洗わなくてはいけません。'"
@@ -396,7 +396,7 @@
           ・菜
         </p>
         <SentenceFormat
-          :sentence="'台南菜是甜的.'"
+          :sentence="'台南菜是甜的。'"
           :bopomofo="'ㄊㄞˊ ㄋㄢˊ ㄘㄞˋ ㄕˋ ㄊㄧㄢˊ ㄉㄜ˙.'"
           :pinyin="'tai2 nan2 cai4 shi4 tian2 de.'"
           :translation="'台南料理は甘いです。'"
@@ -406,9 +406,9 @@
           ・越南
         </p>
         <SentenceFormat
-          :sentence="'我喜歡越南菜, 但是那有香菜.'"
-          :bopomofo="'ㄨㄛˇ ㄒㄧˇ ㄏㄨㄢ ㄩㄝˋ ㄋㄢˊ ㄘㄞˋ, ㄉㄢˋ ㄕˋ ㄋㄚˋ ㄧㄡˇ ㄒㄧㄤ ㄘㄞˋ.'"
-          :pinyin="'wo3 xi3 huan1 yue4 nan2 cai4, dan4 shi4 na4 you3 xiang1 cai4.'"
+          :sentence="'雖然越南菜常有香菜, 我還算喜歡。'"
+          :bopomofo="'ㄙㄨㄟ ㄖㄢˊ ㄩㄝˋ ㄋㄢˊ ㄘㄞˋ ㄔㄤˊ ㄧㄡˇ ㄒㄧㄤ ㄘㄞˋ, ㄨㄛˇ ㄏㄞˊ ㄙㄨㄢˋ ㄒㄧˇ ㄏㄨㄢ.'"
+          :pinyin="'sui1 ran2 yue4 nan2 cai4 chang2 you3 xiang1 cai4, wo3 hai2 suan4 xi3 huan1.'"
           :translation="'私はベトナム料理が好きですが、 パクチーが入っています。'"
         />
         <br>
