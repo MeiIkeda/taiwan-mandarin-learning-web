1b56a4ccef0661caddbf13a733f38d3845fa3bba
MeiIkeda <mmeeddeecchhii@yahoo.co.jp>
2020-12-30 15:59:17 +0900

    fix

--- pages/sentence/index.vue
+++ pages/sentence/index.vue
@@ -112,6 +112,10 @@
           <nuxt-link :to="{ name: 'sentence-text2-lesson6' }" style="text-align: center">
             當代中文課程課2・第6章
           </nuxt-link>
+          <br>
+          <nuxt-link :to="{ name: 'sentence-text2-lesson7' }" style="text-align: center">
+            當代中文課程課2・第7章
+          </nuxt-link>
         </p>
         <br>
         <p class="text-itself-about">
